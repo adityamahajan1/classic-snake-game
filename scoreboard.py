a4206d2048e1754dc5edf28019a21f79216361ed
aditya mahajan <adityamahajan1@gmail.com>
2022-03-30 12:34:14 -0400

    added logic to persist high score

--- scoreboard.py
+++ scoreboard.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+
 ALIGN = "center"
 FONT = ("Arial", 16, "normal")
 
@@ -11,20 +12,27 @@ class ScoreBoard(Turtle):
         self.color("white")
         self.goto(0, 270)
         self.score = 0
-        self.high_score = 0
+
+        try:
+            with open("data.txt") as file:
+                self.high_score = int(file.read())
+        except:
+            self.high_score = 0
+
         self.update_scoreboard()
 
     def update_scoreboard(self):
         self.clear()
         self.write(f"Score: {self.score}  High Score: {self.high_score}", False, align=ALIGN, font=FONT)
 
-    # def game_over(self):
-    #     self.goto(0,0)
-    #     self.write("Game over", False, align=ALIGN, font=FONT)
-
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            try:
+                with open("data.txt", "w") as file:
+                    file.write(str(self.high_score))
+            except:
+                file.write(str(0))
         self.score = 0
         self.update_scoreboard()
 
